5b3d5c81fa556492b2d20e7e5cd62784c0548899
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2023-01-10 11:54:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-//https://teachablemachine.withgoogle.com/models/f-dbRjIQz/
+//https://teachablemachine.withgoogle.com/models/f-dbRjIQz/ //object rec
+
+//https://teachablemachine.withgoogle.com/models/YSaCKb_EJ/ //face rec
+
+Webcam.set(
+    {
+       width:350,
+       height:350,
+       image_format:'png',
+       png_quality:90
+    }
+);
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5.version',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/f-dbRjIQz/model.json',modelLoaded);
+ function modelLoaded(){
+    console.log('modelLoaded')
+ }
+
+ function check(){
+    img=document.getElementById('capture_img');
+    classifier.classify(img, gotResult);
+ }
+
+ function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+ }
